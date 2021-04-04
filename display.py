18bc837ed89b0c8c9d04d98a555935de76651dd5
Setu Shah <setu4993@yahoo.co.in>
2021-04-03 20:05:34 -0700

    Store RSS feed output in streamlit cache

--- display.py
+++ display.py
@@ -5,5 +5,10 @@ from streamlit import title, selectbox, markdown, cache, set_page_config
 
 from seen_unseen_books.parse_feed import fetch_books_from_feed
 
+
+@cache(ttl=timedelta(days=1).total_seconds())
+def books_episodes():
+    return fetch_books_from_feed(5)
+
 if __name__ == "__main__":
     create_page()
