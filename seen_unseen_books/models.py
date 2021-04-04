58bbe06692647739e8ae588974753748946df579
Setu Shah <setu4993@yahoo.co.in>
2021-04-03 20:21:13 -0700

    Add markdown methods to models

--- seen_unseen_books/models.py
+++ seen_unseen_books/models.py
@@ -7,13 +7,20 @@ class Episode:
     title: str
     url: str
 
+    def md(self) -> str:
+        return f"[{self.title}]({self.url})"
+
 
 @dataclass
 class Episodes:
     episodes: List[Episode] = field(default_factory=list)
 
-    def __len__(self):
+    def __len__(self) -> int:
         return len(self.episodes)
+
+    def md(self, tabbed: bool = True) -> List[str]:
+        prefix = "    " if tabbed else ""
+        return [f"{prefix}- {episode.md()}" for episode in self.episodes]
 
 
 @dataclass
@@ -21,3 +28,6 @@ class Book:
     title: str
     author: str
     url: Optional[str] = None
+
+    def md(self) -> str:
+        return f"[{self.title} by {self.author}]({self.url})"
